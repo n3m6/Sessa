2d8b56cd2ab4817f9979da0fe6839ff0883e1ae3
Abdulla Faraz <abdulla.faraz@gmail.com>
2017-09-09 23:28:05 +0500

    eslint support for development

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
 const config = require('./config');
 const bitmexClient = require('bitmex-realtime-api');
-const client = new bitmexClient({testnet: true,
+
+const client = new bitmexClient({
+  testnet: true,
   apiKeyID: config.api.key,
-  apiKeySecret: config.api.secret
+  apiKeySecret: config.api.secret,
 });
 
-client.addStream('XBTUSD', 'instrument', function(data, symbol, tableName) {
-  console.log(symbol + " " + data);
+client.addStream('XBTUSD', 'instrument', (data, symbol, tableName) => {
+  console.log(`${symbol} ${data}`);
 });
