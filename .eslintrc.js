04b11ea9b771af6ece6f44ba993158f82a63cd72
Abdulla Faraz <abdulla.faraz@gmail.com>
2017-09-10 18:12:29 +0500

    ignore linebreak (LF/CRLF) warnings

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,7 @@
 module.exports = {
-    "extends": "airbnb-base",
-    "rules": {
-      "no-console": 0
-    }
+  extends: 'airbnb-base',
+  rules: {
+    'no-console': 0,
+    'linebreak-style': 0,
+  },
 };
