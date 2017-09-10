211eb1abbd8604533d58c74a73857b971789d05b
Abdulla Faraz <abdulla.faraz@gmail.com>
2017-09-10 17:22:20 +0500

    testing bitmex-realtime-api lib

--- main.js
+++ main.js
@@ -1,12 +1,40 @@
-const config = require('./config');
+// const config = require('./config');
 const BitmexClient = require('bitmex-realtime-api');
 
+// TODO: Remove BitMex's library and use another library for websockets.
+
 const client = new BitmexClient({
-  testnet: true,
-  apiKeyID: config.api.key,
-  apiKeySecret: config.api.secret,
+  testnet: false,
+  // apiKeyID: config.api.key,
+  // apiKeySecret: config.api.secret,
 });
 
-client.addStream('XBTUSD', 'instrument', (data, symbol, tableName) => {
-  console.log(`${symbol} ${data} ${tableName}`);
+client.on('error', console.error);
+// client.on('open', () => console.log('connection opened.'));
+// client.on('close', () => console.log('connecttion closed.'));
+// client.on('initialize', () => console.log('initialized, waiting for data'));
+
+// console.log('Timestamp\t\t\tOpen\tHigh\tLow\tClose\tTrades\tVolume\tAlength');
+/* client.addStream('XBTUSD', 'tradeBin1m', (data) => {
+  const info = data[data.length - 1];
+  console.log(`${info.timestamp}\t${
+    info.open}\t${
+    info.high}\t${
+    info.low}\t${
+    info.close}\t${
+    info.trades}\t${
+    info.volume}\t${
+    data.length}`);
+}); */
+
+console.log('Timestamp\t\t\tSide\tSize\tPrice\tAlength');
+client.addStream('XBTUSD', 'trade', (data) => {
+  const info = data[data.length - 1];
+  console.log(`${info.timestamp}\t${
+    info.side}\t${
+    info.size}\t${
+    info.price}\t${
+    data.length}`);
+  // console.log(JSON.stringify(data));
+  // console.log('\n');
 });
