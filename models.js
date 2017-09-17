d8568b8ba1f9dc69b108ae90337d7411be628fb2
Abdulla Faraz <abdulla.faraz@gmail.com>
2017-09-18 04:17:40 +0500

    Added order id to positions model

--- models.js
+++ models.js
@@ -1,6 +1,7 @@
 const positions = {
   XBTUSD: {
     activeTrade: false, // is there an active trade with this currency
+    orderID: '',
     orderType: '', // long or short
     contracts: 0, // number of contracts bought
     value: 0, // total value of contracts in play
