682f9d9b6183ddeacd9e4555f2306db929d5d301
Abdulla Faraz <abdulla.faraz@gmail.com>
2017-09-17 05:24:53 +0500

    using unirest for rest client operations

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const config = require('./config');
 const engine = require('./engine').Engine;
+const unirest = require('unirest');
 const BitmexClient = require('bitmex-realtime-api');
 
 const client = new BitmexClient(config.bitmexConfig);
@@ -9,8 +10,8 @@ client.on('open', () => console.log('connection opened.'));
 client.on('close', () => console.log('connecttion closed.'));
 client.on('initialize', () => console.log('initialized, waiting for data'));
 
-console.log('#\tTimestamp\t\t\tClose\tVolume\tMA\tRSI\tMACD\tPstn\tType');
-client.addStream('XBTUSD', 'tradeBin1m', data => engine.oneMinuteProcessing(data));
+// console.log('#\tTimestamp\t\t\tClose\tVolume\tMA\tRSI\tMACD\tPstn\tType');
+// client.addStream('XBTUSD', 'tradeBin1m', data => engine.oneMinuteProcessing(data));
 
 /* client.addStream('XBTUSD', 'execution', (data) => {
   console.log('execution update');
