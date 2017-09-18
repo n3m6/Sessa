9bfe792fd253aa2a5c66a9b4808cab80474d599d
Abdulla Faraz <abdulla.faraz@gmail.com>
2017-09-19 02:11:47 +0500

    added BitMex identifier, compartmentalizing for more future exchanges

--- models.js
+++ models.js
@@ -1,8 +1,9 @@
 const positions = {
   XBTUSD: {
-    activeTrade: false, // is there an active trade with this currency
-    orderID: '',
-    orderType: '', // long or short
+    activeTrade: false, // is there an active trade with this curr/commodity
+    exchange: 'BitMEX',
+    orderID: '', // unique identifier, will be used in tdb able as well
+    orderType: '', // LONG or SHORT (not Buy or Sell)
     contracts: 0, // number of contracts bought
     value: 0, // total value of contracts in play
   },
