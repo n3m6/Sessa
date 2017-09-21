15fdf36196330c15ef0eb7a9166b4dd069b81587
Abdulla Faraz <abdulla.faraz@gmail.com>
2017-09-21 09:44:56 +0500

    code refactoring

--- bitmex.js
+++ bitmex.js
@@ -4,6 +4,24 @@ const crypto = require('crypto');
 
 // FIXME refactor this library get rid of duplicate parts in the functions
 
+function bmHeaders(verb, path, postBody) {
+  const expires = Date.now() + 60000;
+  const signature = crypto
+    .createHmac('sha256', config.api.secret)
+    .update(verb + path + expires + postBody)
+    .digest('hex');
+
+  const headers = {
+    'content-type': 'application/json',
+    Accept: 'application/json',
+    'X-Requested-With': 'XMLHttpRequest',
+    'api-expires': expires,
+    'api-key': config.api.key,
+    'api-signature': signature,
+  };
+  return headers;
+}
+
 const BitMEX = function BitMEX() {};
 
 BitMEX.prototype.init = function init() {};
@@ -12,21 +30,7 @@ BitMEX.prototype.getBitMexBalance = function getBitMexBalance() {
   return new Promise((resolve, reject) => {
     const verb = 'GET';
     const path = '/api/v1/user/wallet';
-    const expires = Date.now() + 60000;
-
-    const signature = crypto
-      .createHmac('sha256', config.api.secret)
-      .update(verb + path + expires)
-      .digest('hex');
-
-    const headers = {
-      'content-type': 'application/json',
-      Accept: 'application/json',
-      'X-Requested-With': 'XMLHttpRequest',
-      'api-expires': expires,
-      'api-key': config.api.key,
-      'api-signature': signature,
-    };
+    const headers = bmHeaders(verb, path, '');
 
     const request = unirest.get(config.api.resthost + path);
     request.header(headers).end((response) => {
@@ -40,25 +44,12 @@ BitMEX.prototype.bitMexAdjustMargin = function bitMexAdjustMargin(margin) {
   return new Promise((resolve, reject) => {
     const verb = 'POST';
     const path = '/api/v1/position/leverage';
-    const expires = Date.now() + 60000;
     const data = {
       symbol: 'XBTUSD',
       leverage: margin,
     };
     const postBody = JSON.stringify(data);
-    const signature = crypto
-      .createHmac('sha256', config.api.secret)
-      .update(verb + path + expires + postBody)
-      .digest('hex');
-
-    const headers = {
-      'content-type': 'application/json',
-      Accept: 'application/json',
-      'X-Requested-With': 'XMLHttpRequest',
-      'api-expires': expires,
-      'api-key': config.api.key,
-      'api-signature': signature,
-    };
+    const headers = bmHeaders(verb, path, postBody);
 
     const request = unirest.post(config.api.resthost + path);
     request
@@ -75,7 +66,6 @@ BitMEX.prototype.bitMexMarketOrder = function bitMexMarketOrder(side, orderQty) 
   return new Promise((resolve, reject) => {
     const verb = 'POST';
     const path = '/api/v1/order';
-    const expires = Date.now() + 60000;
     const data = {
       symbol: 'XBTUSD',
       side,
@@ -84,19 +74,7 @@ BitMEX.prototype.bitMexMarketOrder = function bitMexMarketOrder(side, orderQty) 
       timeInForce: 'ImmediateOrCancel',
     };
     const postBody = JSON.stringify(data);
-    const signature = crypto
-      .createHmac('sha256', config.api.secret)
-      .update(verb + path + expires + postBody)
-      .digest('hex');
-
-    const headers = {
-      'content-type': 'application/json',
-      Accept: 'application/json',
-      'X-Requested-With': 'XMLHttpRequest',
-      'api-expires': expires,
-      'api-key': config.api.key,
-      'api-signature': signature,
-    };
+    const headers = bmHeaders(verb, path, postBody);
 
     const request = unirest.post(config.api.resthost + path);
     request
@@ -110,90 +88,70 @@ BitMEX.prototype.bitMexMarketOrder = function bitMexMarketOrder(side, orderQty) 
 };
 
 BitMEX.prototype.bitMexClosePosition = function bitMexClosePosition(orderId) {
-  const verb = 'POST';
-  const path = '/api/v1/order';
-  const expires = Date.now() + 60000;
-  const data = {
-    orderID: orderId,
-    symbol: 'XBTUSD',
-    execInst: 'Close',
-  };
-  const postBody = JSON.stringify(data);
-  const signature = crypto
-    .createHmac('sha256', config.api.secret)
-    .update(verb + path + expires + postBody)
-    .digest('hex');
+  return new Promise((resolve, reject) => {
+    const verb = 'POST';
+    const path = '/api/v1/order';
+    const data = {
+      orderID: orderId,
+      symbol: 'XBTUSD',
+      execInst: 'Close',
+    };
+    const postBody = JSON.stringify(data);
+    const headers = bmHeaders(verb, path, postBody);
 
-  const headers = {
-    'content-type': 'application/json',
-    Accept: 'application/json',
-    'X-Requested-With': 'XMLHttpRequest',
-    'api-expires': expires,
-    'api-key': config.api.key,
-    'api-signature': signature,
-  };
-
-  const request = unirest.post(config.api.resthost + path);
-  request
-    .header(headers)
-    .send(postBody)
-    .end((response) => {
-      if (response.code === 200) {
-        // console.log(response.body);
-        // return resolve(response);
-        // FIXME this could be better
-        console.log('position closed');
-      } else {
+    const request = unirest.post(config.api.resthost + path);
+    request
+      .header(headers)
+      .send(postBody)
+      .end((response) => {
+        if (response.code === 200) {
+          console.log('position closed');
+          return resolve(response);
+        }
         console.log('position could not be closed');
-      }
-      // console.log(`error: ${JSON.stringify(response.body)}`);
-      // return reject(response);
-    });
+        return reject(response);
+      });
+  });
 };
 
 // FIXME test stop loss function (haven't been tested yet)
 BitMEX.prototype.bitMexSetStopLoss = function bitMexSetStopLoss(orderId, stopPrice) {
-  const verb = 'POST';
-  const path = '/api/v1/order';
-  const expires = Date.now() + 60000;
-  const data = {
-    orderID: orderId,
-    symbol: 'XBTUSD',
-    ordType: 'Stop',
-    stopPx: stopPrice,
-    execInst: 'Close, LastPrice',
-  };
-  const postBody = JSON.stringify(data);
-  const signature = crypto
-    .createHmac('sha256', config.api.secret)
-    .update(verb + path + expires + postBody)
-    .digest('hex');
+  return new Promise((resolve, reject) => {
+    const verb = 'POST';
+    const path = '/api/v1/order';
+    const data = {
+      orderID: orderId,
+      symbol: 'XBTUSD',
+      ordType: 'Stop',
+      stopPx: stopPrice,
+      execInst: 'Close, LastPrice',
+    };
+    const postBody = JSON.stringify(data);
+    const headers = bmHeaders(verb, path, postBody);
 
-  const headers = {
-    'content-type': 'application/json',
-    Accept: 'application/json',
-    'X-Requested-With': 'XMLHttpRequest',
-    'api-expires': expires,
-    'api-key': config.api.key,
-    'api-signature': signature,
-  };
-
-  const request = unirest.post(config.api.resthost + path);
-  request
-    .header(headers)
-    .send(postBody)
-    .end((response) => {
-      if (response.code === 200) {
-        // console.log(response.body);
-        // return resolve(response);
-        // FIXME this could be better
-        console.log('stop loss set');
-      } else {
+    const request = unirest.post(config.api.resthost + path);
+    request
+      .header(headers)
+      .send(postBody)
+      .end((response) => {
+        if (response.code === 200) {
+          console.log('stop loss set');
+          return resolve(response);
+        }
         console.log(`could not set stop loss for position${JSON.stringify(response)}`);
-      }
-      // console.log(`error: ${JSON.stringify(response.body)}`);
-      // return reject(response);
-    });
+        return reject(response);
+      });
+  });
 };
 
 exports.BitMEX = new BitMEX();
+
+/* to test:
+bitMexSetStopLoss
+bitMexClosePosition
+bitMexMarketOrder
+bitMexAdjustMargin
+getBitMexBalance
+*/
+
+// const bm = new BitMEX();
