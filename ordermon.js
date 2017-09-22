49b52df699437cb62de3c5db08ca3cb6c928a029
Abdulla Faraz <abdulla.faraz@gmail.com>
2017-09-22 06:23:59 +0500

    shows client order id if ther is one

--- ordermon.js
+++ ordermon.js
@@ -10,11 +10,13 @@ client.on('open', () => console.log('connection opened.'));
 client.on('close', () => console.log('connecttion closed.'));
 client.on('initialize', () => console.log('initialized, waiting for data'));
 
-console.log('#\tSide\tQty\tPrice\tType\tStatus\t\tavgPrce\torderID');
+console.log('#\tSide\tQty\tPrice\tType\tStatus\t\tavgPrce\torderID\tclOID');
 client.addStream('XBTUSD', 'order', (data) => {
   const curr = data.length - 1;
   if (data.length > 0) {
     console.log(`${data.length}\t${data[curr].side}\t${data[curr].orderQty}\t${data[curr].price}\t${data[curr]
-      .ordType}\t${data[curr].ordStatus}\t\t${data[curr].avgPx}\t${data[curr].orderID}`);
+      .ordType}\t${data[curr].ordStatus}\t\t${data[curr].avgPx}\t${data[curr].orderID}\t${data[
+      curr
+    ].clOrdID}`);
   }
 });
