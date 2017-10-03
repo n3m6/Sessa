c1319b22ccb98d73a1064f583d863747e2b8f483
Abdulla Faraz <abdulla.faraz@gmail.com>
2017-10-03 17:07:08 +0500

    Added 5 minute processing to delta server

--- delta/deltaRecord.js
+++ delta/deltaRecord.js
@@ -1,12 +1,55 @@
 const config = require('../config');
 const db = require('./deltadb.js').DeltaDB;
 const df = require('./deltaFinancial.js').DeltaFinancial;
+const utils = require('../utils');
 
 const DeltaRecord = function DeltaRecord() {
   console.log('TIME\t\t\t\tOPEN\tHIGH\tLOW\tCLOSE\tTRADES\tVOL\tVWAP\tSMA30\tRSI\tRSI_GA\tRSI_LO\tMACD12\tMACD26\tSGNL\tMACD');
 };
 
 const bitmex1MinPrefix = config.bitmex1MinPrefix; // eslint-disable-line
+
+function getHigh(data) {
+  let high = 0;
+  for (let i = 0; i < data.length; i += 1) {
+    high = data[i].high > high ? data[i].high : high;
+  }
+  return high;
+}
+
+function getLow(data) {
+  let low = data[0].low; // eslint-disable-line
+  for (let i = 0; i < data.length; i += 1) {
+    low = data[i].low < low ? data[i].low : low;
+  }
+  return low;
+}
+
+function getTrades(data) {
+  let trades = 0;
+  for (let i = 0; i < data.length; i += 1) {
+    trades += parseInt(data[i].trades, 10);
+  }
+  return trades;
+}
+
+function getVolume(data) {
+  let volume = 0;
+  for (let i = 0; i < data.length; i += 1) {
+    volume += parseInt(data[i].volume, 10);
+  }
+  return volume;
+}
+
+function fiveMinuteProcessing(lastFive) {
+  const openFive = lastFive[0].open;
+  const highFive = getHigh(lastFive);
+  const lowFive = getLow(lastFive);
+  const closeFive = lastFive[lastFive.length - 1].close;
+  const tradesFive = getTrades(lastFive);
+  const volumeFive = getVolume(lastFive);
+  return [openFive, highFive, lowFive, closeFive, tradesFive, volumeFive];
+}
 // set up simple get/set for db values
 DeltaRecord.prototype.process = function process(data) {
   const lastCandle = data[data.length - 1];
@@ -31,7 +74,7 @@ DeltaRecord.prototype.process = function process(data) {
       // console.log('records are empty');
       // IF records are empty start entering data
 
-      let lastTime = '';
+      let lastTime = ''; // timestamp of the last entry in db
       try {
         if (response[response.length - 1].timestamp !== null) {
           lastTime = response[response.length - 1].timestamp;
@@ -39,10 +82,11 @@ DeltaRecord.prototype.process = function process(data) {
       } catch (e) {
         lastTime = '';
       }
+
+      // IF it's a new record not an entry that already exists
       if (parseInt(nixtime, 10) !== parseInt(lastTime, 10)) {
         const sma30 = df.sma(response, 30, close);
-        const [avggain, avgloss, rsi] = df.rsi(response, config.rsi, lastCandle);
-
+        const [rsiavggain, rsiavgloss, rsi] = df.rsi(response, config.rsi, lastCandle);
         const [mema12, mema26, msignal, macd] = df.macd(
           response,
           config.macd.line1,
@@ -62,14 +106,77 @@ DeltaRecord.prototype.process = function process(data) {
           vwap,
           sma30,
           rsi,
-          avggain,
-          avgloss,
+          rsiavggain,
+          rsiavgloss,
           mema12,
           mema26,
           msignal,
           macd,
         };
+        // insert into db
         db.insert1min(args).catch(console.error);
+
+        // Processing trades at 5 min bins
+        if (jsDate.getMinutes() % 5 === 0) {
+          const lastFive = utils.arraySlice(4, response);
+          lastFive.push(args);
+
+          // console.log(JSON.stringify(lastFive));
+          const [
+            openFive,
+            highFive,
+            lowFive,
+            closeFive,
+            tradesFive,
+            volumeFive,
+          ] = fiveMinuteProcessing(lastFive);
+
+          db
+            .get5MinLast50()
+            .then((responseFive) => {
+              const lastCandleFive = {
+                open: openFive,
+                high: highFive,
+                low: lowFive,
+                close: closeFive,
+              };
+              const sma30Five = df.sma(responseFive, 30, closeFive);
+              const [rsigainFive, rsilossFive, rsiFive] = df.rsi(
+                responseFive,
+                config.rsi,
+                lastCandleFive,
+              );
+
+              const [mema12Five, mema26Five, msignalFive, macdFive] = df.macd(
+                responseFive,
+                config.macd.line1,
+                config.macd.line2,
+                config.macd.signal,
+                lastCandleFive,
+              );
+
+              const fiveArgs = {
+                nixtime,
+                openFive,
+                highFive,
+                lowFive,
+                closeFive,
+                tradesFive,
+                volumeFive,
+                sma30Five,
+                rsiFive,
+                rsigainFive,
+                rsilossFive,
+                mema12Five,
+                mema26Five,
+                msignalFive,
+                macdFive,
+              };
+              // insert data
+              db.insert5min(fiveArgs).catch(console.error);
+            })
+            .catch(console.error);
+        }
       }
     })
     .catch(console.error);
--- delta/deltadb.js
+++ delta/deltadb.js
@@ -9,6 +9,7 @@ Ordered Set BitMEX:XBTUSD:1min (key 'timestamp' value 'timestamp')
 use ordered set to range search by time for removal and filter functions
 */
 const bitmex1MinPrefix = config.bitmex1MinPrefix; // eslint-disable-line
+const bitmex5MinPrefix = config.bitmex5MinPrefix; // eslint-disable-line
 
 client.on('error', (err) => {
   console.log(`DB Error. Is DB available? ${err}`);
@@ -20,10 +21,10 @@ const DeltaDB = function DeltaDB() {};
 DeltaDB.prototype.get1MinLast50 = function get1MinLast50() {
   return new Promise((resolve, reject) => {
     // FIXME
-    // const endTime = new Date().getTime();
-    // const startTime = endTime - 3060000; // 51*60*1000 (51 minutes in millisecs)
-    const startTime = '-inf';
-    const endTime = '+inf';
+    const endTime = new Date().getTime();
+    const startTime = endTime - 3060000; // 51*60*1000 (51 minutes in millisecs)
+    // const startTime = '-inf';
+    // const endTime = '+inf';
     const args = [bitmex1MinPrefix, startTime, endTime];
     // console.log(args);
 
@@ -53,35 +54,70 @@ DeltaDB.prototype.get1MinLast50 = function get1MinLast50() {
   });
 };
 
-// FIXME insert a 1min bin
-DeltaDB.prototype.insert1min = function insert1Min(args) {
-  const {
-    nixtime,
-    open,
-    high,
-    low,
-    close,
-    trades,
-    volume,
-    vwap,
-    sma30,
-    rsi,
-    avggain,
-    avgloss,
-    mema12,
-    mema26,
-    msignal,
-    macd,
-  } = args;
+DeltaDB.prototype.get5MinLast50 = function get5MinLast50() {
+  return new Promise((resolve, reject) => {
+    // FIXME
+    const endTime = new Date().getTime();
+    const startTime = endTime - 15300000; // 51*5*60*1000 (255 minutes in millisecs)
+    // const startTime = '-inf';
+    // const endTime = '+inf';
+    const args = [bitmex5MinPrefix, startTime, endTime];
+    // console.log(args);
 
-  /* sma50,
+    // eslint-disable-next-line
+    client.zrangebyscore(args, (zerr, zresponse) => {
+      if (zerr) return reject(zerr);
+      // return resolve(response);
+      // console.log(zresponse);
+      const result = [];
+      (function get5MinHashes() {
+        const record = zresponse.splice(0, 1)[0];
+        // console.log(record);
+
+        // eslint-disable-next-line
+        client.hgetall(`${bitmex5MinPrefix}:${record}`, (err, response) => {
+          if (err) return reject(err);
+          if (zresponse.length === 0) {
+            result.push(response);
+            return resolve(result);
+          }
+          result.push(response);
+          setTimeout(get5MinHashes, 0);
+          // async iteration, using this instead of tail recursion to prevent stack blowout
+        });
+      }());
+    });
+  });
+};
+
+DeltaDB.prototype.insert1min = function insert1Min(args) {
+  return new Promise((resolve, reject) => {
+    const {
+      nixtime,
+      open,
+      high,
+      low,
+      close,
+      trades,
+      volume,
+      vwap,
+      sma30,
+      rsi,
+      rsiavggain,
+      rsiavgloss,
+      mema12,
+      mema26,
+      msignal,
+      macd,
+    } = args;
+
+    /* sma50,
   sma100,
   sma200,
   ema9, */
-  const timestamp = nixtime;
-  const tvwap = vwap === null ? 'null' : utils.roundTo(vwap, config.significant);
+    const timestamp = nixtime;
+    const tvwap = vwap === null ? 'null' : utils.roundTo(vwap, config.significant);
 
-  return new Promise((resolve, reject) => {
     const inargs = [
       `${bitmex1MinPrefix}:${timestamp}`,
       'timestamp',
@@ -105,9 +141,9 @@ DeltaDB.prototype.insert1min = function insert1Min(args) {
       'rsi',
       rsi,
       'rsiavggain',
-      avggain,
+      rsiavggain,
       'rsiavgloss',
-      avgloss,
+      rsiavgloss,
       'mema12',
       mema12,
       'mema26',
@@ -120,7 +156,80 @@ DeltaDB.prototype.insert1min = function insert1Min(args) {
     const zargs = [`${bitmex1MinPrefix}`, `${timestamp}`, timestamp];
     const t = new Date(timestamp);
     const tTime = t.toISOString();
-    console.log(`${tTime}\t${open}\t${high}\t${low}\t${close}\t${trades}\t${volume}\t${tvwap}\t${sma30}\t${rsi}\t${avggain}\t${avgloss}\t${mema12}\t${mema26}\t${msignal}\t${macd}`);
+    console.log(`${tTime}\t${open}\t${high}\t${low}\t${close}\t${trades}\t${volume}\t${tvwap}\t${sma30}\t${rsi}\t${rsiavggain}\t${rsiavgloss}\t${mema12}\t${mema26}\t${msignal}\t${macd}`);
+
+    client
+      .multi()
+      .hmset(inargs)
+      .zadd(zargs)
+      .exec((err, replies) => {
+        if (err) reject(err);
+        resolve(replies);
+      });
+  });
+};
+
+DeltaDB.prototype.insert5min = function insert5min(args) {
+  return new Promise((resolve, reject) => {
+    const {
+      nixtime,
+      openFive,
+      highFive,
+      lowFive,
+      closeFive,
+      tradesFive,
+      volumeFive,
+      sma30Five,
+      rsiFive,
+      rsigainFive,
+      rsilossFive,
+      mema12Five,
+      mema26Five,
+      msignalFive,
+      macdFive,
+    } = args;
+
+    const timestamp = nixtime;
+
+    const inargs = [
+      `${bitmex5MinPrefix}:${timestamp}`,
+      'timestamp',
+      timestamp,
+      'open',
+      openFive,
+      'high',
+      highFive,
+      'low',
+      lowFive,
+      'close',
+      closeFive,
+      'trades',
+      tradesFive,
+      'volume',
+      volumeFive,
+      'sma30',
+      sma30Five,
+      'rsi',
+      rsiFive,
+      'rsiavggain',
+      rsigainFive,
+      'rsiavgloss',
+      rsilossFive,
+      'mema12',
+      mema12Five,
+      'mema26',
+      mema26Five,
+      'msignal',
+      msignalFive,
+      'macd',
+      macdFive,
+    ];
+    const zargs = [`${bitmex5MinPrefix}`, `${timestamp}`, timestamp];
+    const t = new Date(timestamp);
+    const tTime = t.toISOString();
+    console.log('----------- 5 min -----------');
+    console.log(`${tTime}\t${openFive}\t${highFive}\t${lowFive}\t${closeFive}\t${tradesFive}\t${volumeFive}\t\t${sma30Five}\t${rsiFive}\t${rsigainFive}\t${rsilossFive}\t${mema12Five}\t${mema26Five}\t${msignalFive}\t${macdFive}`);
+    console.log('-----------------------------');
 
     client
       .multi()
